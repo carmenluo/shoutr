9d8f40929ea809a4bd0ce842f08bd09058f098b4
carmenluo <Sdluojm@gmail.com>
2019-11-29 22:59:39 -0500

    add create and show shout

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,4 +2,9 @@ class Shout < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length:{in: 1..144}
   validates :user, presence: true
+  delegate :username, to: :user;
+  # we can define it by ourserlf or just like above using delegate
+  # def username
+  #   user.username
+  # end
 end
