395aeba5328c1e76ed6194983125f1097f1d3370
carmenluo <Sdluojm@gmail.com>
2019-11-29 21:23:16 -0500

    update clearance

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,13 @@
 class HomesController < ApplicationController
+  # before_action :redirect_to_dashboard_if_signed_in
   def show
     render
   end
+
+  # def redirect_to_dashboard_if_signed_in
+  #   if signed_in?
+  #     redirect_to dashboard_path
+  #   end
+  # end
+
 end
