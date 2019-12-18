ccf9ab17a132ed74e30523e21c4b6f94a68ce917
carmenluo <Sdluojm@gmail.com>
2019-12-18 18:29:53 -0500

    use paperclip for photo update

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,2 +1,7 @@
 class PhotoShout < ApplicationRecord
+  has_attached_file :image, styles: {thumb: "200*200"}
+  validates_attachment :image,
+  content_type: {content_type: ["image/jpg", "image/gif", "image/png"]},
+  size: {in: 0..10.megabytes},
+  presence: true
 end
